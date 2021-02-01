9deb9d9c061e3036fc407bd5e171fbf3d92215db
namtp185 <namtp185@gmail.com>
2021-02-01 21:39:28 +0700

    add route for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TestingComponent } from './testing/testing.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoggedGuard } from './_guards/logged.guard';
+import { QuizDetailComponent } from './quiz-detail/quiz-detail.component';
+import { ConfirmTestGuard } from './_guards/confirm-test.guard';
+// import { ResultComponent } from './result/result.component';
+
+const routes: Routes = [
+  {
+      path: '', component: DashboardComponent, 
+      // canActivate: [AuthGuard],
+      children: [
+          { 
+            path: 'quiz-detail',
+            component: QuizDetailComponent 
+          },
+          { 
+            path: 'testing/:id',
+            component: TestingComponent,
+            // canActivate: [ConfirmTestGuard] 
+          },
+          { 
+            path: '', redirectTo: 'quiz-detail', pathMatch: 'full', 
+            // canActivate: [AuthGuard] 
+          },
+          // { path: 'result/:id', component: ResultComponent }
+      ]
+  },
+  { 
+    path: 'login', component: LoginComponent, 
+    // canActivate: [LoggedGuard] 
+  },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' },
+  { path: 'about', component: LandingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
